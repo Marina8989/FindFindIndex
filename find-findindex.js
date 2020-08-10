4f871b3556d1fd461e35fce39c193b29b9e91251
Marina8989 <marina.mill209@gmail.com>
2020-08-10 00:07:12 -0700

    finished

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,22 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(function (item) {
+    if (item['username'] === username) {
+      return item;
+    }
+  });
+
+}
+const users = [
+  { username: 'mlewis' },
+  { username: 'akagen' },
+  { username: 'msmith' }
+];
+
+console.log(findUserByUsername(users, 'mlewis')) // {username: 'mlewis'}
+console.log(findUserByUsername(users, 'taco')) // undefined
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +42,19 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  const x = usersArray.findIndex(function (item) {
+    return item.username === username;
+  });
+  if (x === -1) return;
+  return usersArray.splice(x, 1)[0];
+}
+
+const newUsers = [
+  { username: 'mlewis' },
+  { username: 'akagen' },
+  { username: 'msmith' }
+];
+
+console.log(removeUser(newUsers, 'akagen')) // {username: 'akagen'}
+console.log(removeUser(newUsers, 'akagen')) // undefined
